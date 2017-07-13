eaad3c31ba6c78ff167f71cbdf89fa99275cb071
zhouqitong <zhouqitong@swipal.com>
2017-07-13 08:18:33 +0800

    day2

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,27 @@
 
 <template>
-  <p> world</p>
+   <div>
+     {{hello}}
+     {{numberToDo}} <!--只支持驼峰式写法-->
+
+     {{myValue}}
+   </div>
 </template>
+<script>
+  export default {
+//      props:['number'],
+    props:{
+      "number-to-do": [Number,String,Object],
+      "my-value":[String]
+    },
+    data(){
+          return {
+              hello :"i am component hello "
+          }
+    }
+  }
+
+</script>
+
 
 
